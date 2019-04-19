6726ec8fc27a3aa05037810797bdf58a4f2429f4
Sébastien Saunier <seb@saunier.me>
2019-04-19 10:18:15 +0200

    Refactor API logic to get woeid

--- program.py
+++ program.py
@@ -4,14 +4,15 @@ import requests
 
 BASE_URL = "https://www.metaweather.com"
 
+def get_city_weoid(city):
+    url = f"{BASE_URL}/api/location/search/?query={city}"
+    response = requests.get(url)
+    data = response.json()
+    woeid = data[0]['woeid']
+    return woeid
+
 city = input("Which city do you want to know the weather for?\n> ")
-
-url = f"{BASE_URL}/api/location/search/?query={city}"
-response = requests.get(url)
-data = response.json()
-woeid = data[0]['woeid']
-
-url = f"{BASE_URL}/api/location/{woeid}/"
+url = f"{BASE_URL}/api/location/{get_city_weoid(city)}/"
 response = requests.get(url)
 data = response.json()
 
