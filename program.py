cf6bddd3ab406f752d851f57cf5ea4aff7630941
Sébastien Saunier <seb@saunier.me>
2019-04-19 09:54:46 +0200

    Ask a nicer question to the user

--- program.py
+++ program.py
@@ -4,7 +4,7 @@ import requests
 
 BASE_URL = "https://www.metaweather.com"
 
-city = input("City?\n> ")
+city = input("Which city do you want to know the weather for?\n> ")
 
 url = f"{BASE_URL}/api/location/search/?query={city}"
 response = requests.get(url)
